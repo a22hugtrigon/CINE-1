ea1eb54913ad2c0c3875e0823808413c924c1db9
a22hugtrigon <a22hugtrigon@inspedralbes.cat>
2024-03-13 19:03:42 +0100

    funciona

--- nuxt/pages/listadoPeliculas.vue
+++ nuxt/pages/listadoPeliculas.vue
@@ -16,15 +16,17 @@ import Peliculas from '~/components/Peliculas.vue';
             };
         },
         async mounted() {
-      try {
+    try {
         const res = await fetch('http://127.0.0.1:8000/api/peliculas');
+        if (!res.ok) {
+            throw new Error(`Error al obtener las películas: ${res.status} - ${res.statusText}`);
+        }
         const data = await res.json();
         this.peliculas = data;
-
-      } catch (error) {
-        console.error('Error al obtener las películas', error);
-      }
-    },
+    } catch (error) {
+        console.error('Error al obtener las películas', error.message);
+    }
+}
   };
 </script>
 
